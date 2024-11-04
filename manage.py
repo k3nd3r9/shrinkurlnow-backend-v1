02856d2e0ee5da1ca1f9594fac98b3900c4a5228
ben <ben@flintfreelancer.com>
2024-11-04 15:58:54 -0600

    clean up GET method. Update gunicorn. change sunApp to camelCase

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunapp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sunApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
